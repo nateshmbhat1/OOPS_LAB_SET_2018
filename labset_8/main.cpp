d22ec4ccfce07915b989295df83eec83c46a2ac8
nateshmbhat <nateshmbhat1@gmail.com>
2018-04-12 23:44:35 +0530

    fixed labset_8

--- labset_8/main.cpp
+++ labset_8/main.cpp
@@ -9,16 +9,21 @@ int main(void)
     string file1 , file2 ; 
     fstream ifile1("file1" , ios::in ) , ifile2("file2" , ios::in)   , ofile("merge.txt" , ios::out) ; 
 
-    while(!ifile1.eof())
+    while(ifile1.eof()==false &&  ifile2.eof()==false)
     {
         ifile1 >> file1 ; 
-        ofile << file1 <<endl; ; 
-    } 
-
-    while(!ifile2.eof())
-    {
         ifile2 >> file2 ; 
-        ofile << file2  <<endl; ; 
+        ofile << file1 <<endl; ; 
+        ofile << file2 <<endl; 
     } 
 
+    while(ifile1.eof()==false)
+    {
+        ifile1 >> file1 ; ofile << file1 <<endl; 
+    }
+
+    while(ifile2.eof()==false)
+    {
+        ifile2 >> file2 ; ofile << file2 <<endl; 
+    }
 }
