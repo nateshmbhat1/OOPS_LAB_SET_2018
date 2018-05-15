5472b520043a63f0232cd68fca4b84dadd2f5e38
nateshmbhat <nateshmbhat1@gmail.com>
2018-05-15 09:22:51 +0530

    added labset 1

--- labset_1/main.cpp
+++ labset_1/main.cpp
@@ -1,61 +1,42 @@
-#include<iostream>
-#include<cstdlib>
+#include<bits/stdc++.h>
+using namespace std; 
 
-
-using namespace std ; 
-
-
-class RainGuage
+class Circle
 {
-    protected : 
-    int cm ; 
-    string name ; 
+    private : 
+    int x, y ; 
+    int r ; 
 
     public : 
-
-    RainGuage() ; 
-    void fnReadMeasurement() ; 
-    void fnDispReading() ; 
-    friend void findAvg(const RainGuage * , const int &) ; 
+        Circle(){r =0 ; x = y = 0; }
+        Circle(int rad , int xvar , int yvar):r(rad) , x(xvar) , y(yvar){}
+        friend void operator==(const Circle& c1 , const Circle& c2) ; 
+        // ~Circle(){cout<<"Destructing object !" ; }
+        friend double dist(const Circle& c1 ,const Circle& c2) ; 
 };
 
-void findAvg(const RainGuage * objs , const int& n)
+double dist(const Circle& c1 ,const Circle& c2)
 {
-    int sum =0 ; 
-    for(int i =0 ; i<n ; i++) 
-    {
-        sum+=objs[i].cm ; 
-    }
-
-    cout<<" Average rainfall is " <<sum/n ; 
+    int x1 = c1.x , y1 = c1.y , x2 = c2.x , y2 = c2.y ; 
+    return sqrt((x2-x1)*(x2-x1) +(y2-y1) *(y2-y1)) ; 
 }
 
-RainGuage::RainGuage():cm(0) , name("") {}
+void operator==(const Circle& c1 , const Circle& c2){
 
-void RainGuage::fnDispReading()
-{
-    cout<<" Rain cm = " <<this->cm << " city = " <<this->name <<endl; 
-}
+if(c1.x ==c2.x && c1.y == c2.y)
+    cout<<endl<<"Concentric " <<endl; 
 
-void RainGuage::fnReadMeasurement()
-{
-    cout<<"Enter name of city : " ;
-    cin>>name; 
-    cm = random()%21 ; 
+else if (dist(c1 , c2)> c1.r+c2.r)
+    cout<<"Disjoint !"<<endl; 
+else if(dist(c1 , c2) < c1.r+c2.r)
+    cout<<"Intersecting" <<endl; 
+else
+    cout<<"Touching\n" ; 
+
 }
 
 int main()
 {
-    srand(time(NULL)) ; 
-    int n; 
-    cout<<"Enter number of cities : " ; cin>>n ; 
-    RainGuage * arr = new RainGuage[n] ;  
-    for(int i =0 ; i<n ; i++)
-        arr[i].fnReadMeasurement() ; 
-    
-    for(int i=0 ; i<n ; i++ )
-        arr[i].fnDispReading() ; 
-
-    findAvg(arr , n) ; 
-
+    Circle c1(2 , 0 , 0) , c2(2 ,4 , 0) ; 
+    c1==c2 ; 
 }
